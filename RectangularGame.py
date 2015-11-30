16884e563e82e62b25ae4079959ee5a836d9f93a
karadalex <karadalex@gmail.com>
2015-11-30 23:00:56 +0200

    Changing the whole algorithm
    Change the algorithm so that it gives the solution to time O(N) and not
    O(N^2). All we need is the minimum i and the minimum j of all the pairs
    (i,j) given

--- RectangularGame.py
+++ RectangularGame.py
@@ -1,32 +1,15 @@
 import sys
 
-Grid = [[]]
 N = int(raw_input().strip())
-for n in xrange(N):
+a,b = raw_input().strip().split(' ')
+a,b = [int(a), int(b)]
+iDimension = a
+jDimension = b
+for n in xrange(N-1):
     a,b = raw_input().strip().split(' ')
     a,b = [int(a), int(b)]
-    for i in xrange(a):
-        for j in xrange(b):
-            while True:
-                try:
-                    Grid[i][j] += 1
-                    break
-                except IndexError:
-                    try:
-                        Grid[i].append(0)
-                    except IndexError:
-                        Grid.append([0])
+    iDimension = min(iDimension, a)
+    jDimension = min(jDimension, b)
 
-
-#the cell (1,1) of the board always contains the maximum number
-#searching through all of the entries
-maximum = Grid[0][0]
-numOfMax = 0
-for i in range(len(Grid)):
-    for j in range(len(Grid[i])):
-        if Grid[i][j] == maximum:
-            numOfMax += 1
-        else:
-            break
-
+numOfMax = iDimension*jDimension
 print numOfMax
